f4e13a352f9a0c41f23f3b54d0c8f43574c1789b
Paul Huang <paul19890524@gmail.com>
2023-01-04 01:39:07 +0800

    Add TennisGameTest.cs and A01_LoveAll - fail case

---
+++ TennisKata/TennisKata/TennisGameTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace TennisKata
+{
+    [TestFixture]
+    public class TennisGameTest
+    {
+        [Test]
+        public void A01_LoveAll()
+        {
+            var expected = "Love-All";
+            var tennisGame = new TennisGame();
+            var actual = tennisGame.Score();
+            Assert.AreEqual(actual, expected);
+        }
+    }
+
+    public class TennisGame
+    {
+        public string Score()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
